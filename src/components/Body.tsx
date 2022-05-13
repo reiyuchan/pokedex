928f344936fb25635e622c2de1d77d1ad8cd8fd2
reiyuchan <therealsyedofficial@gmail.com>
2022-05-13 10:24:12 +0200

    added marginTop to main div

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -4,7 +4,7 @@ import './Body.css'
 
 const Body = () => {
     return (
-        <div>
+        <div style={{marginTop: '10rem'}}>
             <h3 style={{ textAlign: 'center', fontSize: '1.5rem', color: 'brown' }}>Welcome to the Pokedex!</h3>
             <div className='randomPokemon'>
                 <div className='pokemon'>
