74d9ce33bf6003b379b8f2b147f217b43f8dbef1
reiyuchan <therealsyedofficial@gmail.com>
2022-05-13 10:25:18 +0200

    added pause audio when tab inactive

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Body from './components/Body'
 import Footer from './components/Footer'
 import LoadMusicOnClick from './utils/LoadMusicOnClick'
 import { generatePokemon } from './PokeAPI/generatepokemon'
+import pauseAudioUnfocus from './utils/pauseaudio'
 
 
 const App = () => {
     useEffect(() => {
         LoadMusicOnClick();
         generatePokemon();
+        pauseAudioUnfocus();
     })
     return (
         <div>
